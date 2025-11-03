84e005502231172fbcca1f26709ebce9a273f34d
Sunly <sonlychao169@gmail.com>
2025-11-03 15:02:23 +0700

    Revert "Add GitHub Actions CI workflow (NPM)"
    This reverts commit a5531aa70728218746247090b4fef433f7c96093.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,20 +3,5 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs}"],
-    plugins: {
-      js,
-    },
-    extends: ["js/recommended"],
-    languageOptions: {
-      ecmaVersion: 2021,
-      sourceType: "module",
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-        ...globals.mocha,
-      },
-    },
-  },
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
 ]);
